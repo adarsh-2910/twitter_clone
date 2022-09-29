fd4cb527d65308f72d6132e69668902f8af714cc
adarsh-2910 <adarsh.shrivastav001@gmail.com>
2022-09-29 18:41:33 +0530

    sign up done

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_09_28_112321) do
+ActiveRecord::Schema[7.0].define(version: 2022_09_29_064913) do
   create_table "twits", force: :cascade do |t|
     t.text "twit"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
